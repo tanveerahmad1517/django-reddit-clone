d4d8660bb8b3072a5057289cb486ae5cdee393c3
anzx42 <ryan@velveetasuicide.com>
2017-09-25 02:17:40 +0000

    Added Comment Rendering to Post View

--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -26,7 +27,31 @@ def getPostContent(post):
     contentToReturn.append(title)
     contentToReturn.append(author)
     contentToReturn.append(content)
+    contentToReturn.append("<hr>")
     return contentToReturn
+
+def getComments(post):
+    DIR = "comments/"
+    stringToReturn = []
+    comments = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
+    for i in range(comments):
+        with open("comments/" + str(i) + ".txt") as f:
+            comment_author = ""
+            comment_content = ""
+            z = 0
+            ignore = False;
+            for line in f:
+                if (z==0):
+                    if (line != str(post)):
+                        ignore = False;
+                if (z == 1 and ignore == False):
+                    comment_author = line
+                if (z > 1 and ignore == False):
+                    comment_content += line
+                z += 1
+            if (ignore==False):
+                stringToReturn.append("<h6><em>" + comment_author + "</h6></em>" + comment_content + "<hr>")
+    return "".join(stringToReturn)
 
 def renderPostPage(post):
     content = getPostContent(post)
@@ -39,8 +64,11 @@ def renderPostPage(post):
                 page.append(content[1])
             elif (line.find("{[Post Content]}") != -1):
                 page.append(content[2])
+            elif (line.find("{[Post Comments]}") != -1):
+                page.append(getComments(post))
             else:
                 page.append(line)
     return "".join(page)
+
 def index(request):
     return HttpResponse(renderPostPage(request.GET['post']))
